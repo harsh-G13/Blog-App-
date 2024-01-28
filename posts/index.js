a40aa27e3e964cef5bffe9a877037e4594de086f
Harsh Goyal <harsh.goyal.cd.eee20@itbhu.ac.in>
2024-01-28 12:21:18 +0530

    included docker and kubernetes

--- posts/index.js
+++ posts/index.js
@@ -12,13 +12,13 @@ app.get('/posts',(req,res)=>{
     res.send(posts); 
 });
 const posts  ={};
-app.post('/posts',async (req,res)=>{
+app.post('/posts/create',async (req,res)=>{
    const id = randomBytes(4).toString('hex');
     const {title} = req.body;
     posts[id]={
         id,title
     };
-   await axios.post('http://localhost:4005/events',{
+   await axios.post('http://event-bus-srv:4005/events',{
         type: 'PostCreated',
         data: {
             id,title
@@ -31,5 +31,5 @@ app.post('/events',(req,res)=>{
     res.send({});
 })
 app.listen(port,()=>{
-    console.log('Listening on 4000');
+    console.log('Hey! Listening on 4000');
 })
--- query/index.js
+++ query/index.js
@@ -41,9 +41,15 @@ app.post('/events',(req,res)=>{
 });
 app.listen(4002,async ()=>{
     console.log("Listening on port 4002");
-    const res = await axios.get('http://localhost:4005/events');
-    for(let event of res.data){
-        console.log('Processing event:' ,event.type)
-        handleEvent(event.type,event.data);
-    }
+    try {
+        const res = await axios.get("http://event-bus-srv:4005/events");
+    
+        for (let event of res.data) {
+          console.log("Processing event:", event.type);
+    
+          handleEvent(event.type, event.data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
 })
